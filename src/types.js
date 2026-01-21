809a24e0c384402b869d53cc71ff97e5f35624a2
SIDHANT BARAPATRE <sidhantbarapatre12@gmail.com>
2026-01-21 15:41:36 +0530

    created metadata.json and types.js

---
+++ src/types.js
@@ -0,0 +1,15 @@
+export const FieldType = {
+  TEXT: 'TEXT',
+  DATE: 'DATE',
+  SIGNATURE: 'SIGNATURE',
+  CHECKBOX: 'CHECKBOX',
+};
+
+export const ContractStatus = {
+  CREATED: 'CREATED',
+  APPROVED: 'APPROVED',
+  SENT: 'SENT',
+  SIGNED: 'SIGNED',
+  LOCKED: 'LOCKED',
+  REVOKED: 'REVOKED',
+};
